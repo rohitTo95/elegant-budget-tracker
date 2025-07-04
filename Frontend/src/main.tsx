d781c8869982fa69af062439dcd989a0d6a0a504
“rohitTo95” <xman76066@gmail.com>
2025-07-05 03:06:35 +0530

    chore: Add comment to main.tsx for clarity

--- Frontend/src/main.tsx
+++ Frontend/src/main.tsx
@@ -2,4 +2,5 @@ import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 // test workflow test 8
+//fi world
 createRoot(document.getElementById("root")!).render(<App />);
