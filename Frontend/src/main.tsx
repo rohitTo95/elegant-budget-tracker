2f3baeb9697071752d20273a0c0709b5b9e4a7ea
“rohitTo95” <xman76066@gmail.com>
2025-07-05 03:05:51 +0530

    fix: Remove public-read ACL from S3 deployment args

--- Frontend/src/main.tsx
+++ Frontend/src/main.tsx
@@ -1,5 +1,5 @@
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
-// test workflow test 2
+// test workflow test 8
 createRoot(document.getElementById("root")!).render(<App />);
