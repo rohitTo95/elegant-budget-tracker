70c29227bd56287e8010515e9577fe7c1c3f71cc
“rohitTo95” <xman76066@gmail.com>
2025-07-05 03:43:10 +0530

    fix: Update AWS credentials configuration in deployment workflow

--- Frontend/src/main.tsx
+++ Frontend/src/main.tsx
@@ -2,5 +2,5 @@ import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 // test workflow test 8
-//hello World 9999
+//hello World 
 createRoot(document.getElementById("root")!).render(<App />);
