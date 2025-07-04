1579b77453304581ad82f92a3ceb0bf2bb68001c
“rohitTo95” <xman76066@gmail.com>
2025-07-05 03:30:33 +0530

    fix: Update CloudFront invalidation process and enhance deployment verification

--- Frontend/src/main.tsx
+++ Frontend/src/main.tsx
@@ -2,5 +2,5 @@ import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 // test workflow test 8
-//hello World
+//hello World 9999
 createRoot(document.getElementById("root")!).render(<App />);
